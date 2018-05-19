662e672775e85b5cb9f6e4f1002bef6f79e93e9e
Jan Syring-Lingenfelder <github@jan-sl.de>
2018-05-19 17:13:47 +0200

    TASK: Remove all the stuff from the starter kit

--- src/actions/app.js
+++ src/actions/app.js
@@ -33,12 +33,6 @@ const loadPage = (page) => async (dispatch) => {
       // Put code here that you want it to run every time when
       // navigate to view1 page and my-view1.js is loaded
       break;
-    case 'view2':
-      await import('../components/my-view2.js');
-      break;
-    case 'view3':
-      await import('../components/my-view3.js');
-      break;
     default:
       page = 'view404';
       await import('../components/my-view404.js');
