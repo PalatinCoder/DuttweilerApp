e781f56af8bd9aa62718b563f42050365cb1f837
Jan Syring-Lingenfelder <github@jan-sl.de>
2018-06-08 22:44:44 +0200

    TASK: Test the news list

---
+++ test/data/news.js
@@ -0,0 +1,96 @@
+export const news_items = 
+[
+{
+	"uid": "node-mg1nt2xofzjff",
+	"headline": "Keller leergepumpt",
+	"subheadline": "Unwetter über Duttweiler",
+	"date": "01.06.2018",
+	"text": "Das durch den Deutschen Wetterdienst (DWD) angekündigte und eingetroffene Unwetter in den frühen Morgenstunden sorgte für rege Einsätze der Feuerwehr. ",
+	"url": "/a-bis-z/c/chronik/chronik-2018/keller-leergepumpt.html"
+}
+    ,
+{
+	"uid": "node-egefsbktsoicx",
+	"headline": "Geinsheim schafft es allein",
+	"subheadline": "Wasser im Keller",
+	"date": "01.06.2018",
+	"text": "Kaum zurück von einer anstrengenden Nacht, gab es erneut einen Alarm wegen einem häuslichen Wasserschaden in Geinsheim.",
+	"url": "/a-bis-z/c/chronik/chronik-2018/geinsheim-schafft-es-allein.html"
+}
+    ,
+{
+	"uid": "node-0l3d4t403tbly",
+	"headline": "Betriebsstoffe ausgelaufen",
+	"subheadline": "Einsatz in Geinsheim",
+	"date": "28.05.2018",
+	"text": "Die Löschgruppe Duttweiler befand sich gerade in einem Weingut beim Üben, als sie zu um 20:19h einem Einsatz in Geinsheim gerufen wurde.",
+	"url": "/a-bis-z/c/chronik/chronik-2018/beriebsstoffe-ausgelaufen.html"
+}
+    ,
+{
+	"uid": "node-qpm49kpzl8nge",
+	"headline": "Hilfe wird nicht benötigt",
+	"subheadline": "Feuerwehr bricht Einsatzfahrt ab",
+	"date": "27.05.2018",
+	"text": "Um 13:53 h wurde der Ausrückebereich Ost zu einem Verkehrsunfall auf der Autobahn gerufen. Vor Ort stellte sich heraus, dass der Löschzug Lachen-Speyerdorf mit ausreichend Personal ausgerückt war, die LG Duttweiler und Geinsheim konnten ihre Alarmfahrt abbrechen.",
+	"url": "/a-bis-z/c/chronik/chronik-2018/hilfe-wird-nicht-benoetigt.html"
+}
+    ,
+{
+	"uid": "node-z323qebb571ox",
+	"headline": "Auf dem Weg zur Deutschen Meisterschaft",
+	"subheadline": "VfL-Schülerinnen gewinnen Regionsmeisterschaft",
+	"date": "26.05.2018",
+	"text": "Die Schülerinnen wurden ihrer Favoritenrolle gerecht und gewannen überlegen das Regionsturnier um den Einzug zum Turnier um die Deutsche Meisterschaft.",
+	"imageUrl": "https://www.duttweiler.de/_Resources/Persistent/1ad5e72846ed33dd6e4ed47bed7d2f5293cdfe09/18-05-27VfL-Sch%C3%BClerinnen-GewinnRegionsmeisterschaft-2.jpg",
+	"imageAlternativeText": "",
+	"url": "/a-bis-z/c/chronik/chronik-2018/vfl-schuelerinnen-auf-dem-weg-zur-deutschen-meisterschaft.html"
+}
+    ,
+{
+	"uid": "node-zeq8eq0ox9l08",
+	"headline": "Unterstützung bei Verkehrsunfall",
+	"subheadline": "Feuerwehr bei schwerem Unfall in Lachen-Speyerdorf",
+	"date": "20.05.2018",
+	"text": "Um 14:23h wurde die Feuerwehr Duttweiler zu einem Verkehrsunfall nach Lachen-Speyerdorf gerufen.",
+	"url": "/a-bis-z/c/chronik/chronik-2018/unterstuetzung-bei-verkehrsunfall.html"
+}
+    ,
+{
+	"uid": "node-y6v0xsbcn502z",
+	"headline": "Unfall auf L540",
+	"subheadline": "Mehrere Fahrzeuge beteiligt",
+	"date": "19.05.2018",
+	"text": "Zwischen Sportplatz und Ortseingang kam es zu einem Unfall mit mehreren Fahrzeugen. Weitere Informationen sind nicht bekannt.",
+	"url": "/a-bis-z/c/chronik/chronik-2018/unfall-auf-l540.html"
+}
+    ,
+{
+	"uid": "node-icmxwd2ugne59",
+	"headline": "Rauchmelder rettet Leben",
+	"subheadline": "Feuerwehr in Geinsheim im Einsatz",
+	"date": "18.05.2018",
+	"text": "Eine Bewohnerin in der Gäustraße  bemerkte am Freitagabend, dass die Rauchwarnmelder bei Ihrem Nachbarn  ausgelöst hatten und alarmierte um 22:16 h die Feuerwehr.",
+	"url": "/a-bis-z/c/chronik/chronik-2018/rauchmelder-rettet-leben.html"
+}
+    ,
+{
+	"uid": "node-endbuxdh8ew3s",
+	"headline": "Feuerwehr feiert",
+	"subheadline": "Zwei Tage Programm",
+	"date": "12.05.2018",
+	"text": "",
+	"url": "/a-bis-z/c/chronik/chronik-2018/feuerwehr-feiert.html"
+}
+    ,
+{
+	"uid": "node-5798c45c0dd9e",
+	"headline": "VDP.Maispitzen",
+	"subheadline": "Jahrgangspräsentation im Weingut Bergdolt",
+	"date": "12.05.2018",
+	"text": "",
+	"imageUrl": "https://www.duttweiler.de/_Resources/Persistent/a8287d236bf637a36e70b3a8e0a6f701f750f189/18-05-12VDP-Maispitzen-Bergdolt.jpg",
+	"imageAlternativeText": "",
+	"url": "/a-bis-z/c/chronik/chronik-2018/vdp-maispitzen.html"
+}
+];
