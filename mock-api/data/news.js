22002766c92aa29946b2f68f9ca6967741e9f12b
Jan Syring-Lingenfelder <github@jan-sl.de>
2018-09-25 18:08:59 +0200

    task(test): new data for the mock-api

--- mock-api/data/news.js
+++ mock-api/data/news.js
@@ -1,96 +1,87 @@
 export const news_items = 
 [
-{
-	"uid": "node-mg1nt2xofzjff",
-	"headline": "Keller leergepumpt",
-	"subheadline": "Unwetter über Duttweiler",
-	"date": "01.06.2018",
-	"text": "Das durch den Deutschen Wetterdienst (DWD) angekündigte und eingetroffene Unwetter in den frühen Morgenstunden sorgte für rege Einsätze der Feuerwehr. ",
-	"url": "/a-bis-z/c/chronik/chronik-2018/keller-leergepumpt.html"
-}
-    ,
-{
-	"uid": "node-egefsbktsoicx",
-	"headline": "Geinsheim schafft es allein",
-	"subheadline": "Wasser im Keller",
-	"date": "01.06.2018",
-	"text": "Kaum zurück von einer anstrengenden Nacht, gab es erneut einen Alarm wegen einem häuslichen Wasserschaden in Geinsheim.",
-	"url": "/a-bis-z/c/chronik/chronik-2018/geinsheim-schafft-es-allein.html"
-}
-    ,
-{
-	"uid": "node-0l3d4t403tbly",
-	"headline": "Betriebsstoffe ausgelaufen",
-	"subheadline": "Einsatz in Geinsheim",
-	"date": "28.05.2018",
-	"text": "Die Löschgruppe Duttweiler befand sich gerade in einem Weingut beim Üben, als sie zu um 20:19h einem Einsatz in Geinsheim gerufen wurde.",
-	"url": "/a-bis-z/c/chronik/chronik-2018/beriebsstoffe-ausgelaufen.html"
-}
-    ,
-{
-	"uid": "node-qpm49kpzl8nge",
-	"headline": "Hilfe wird nicht benötigt",
-	"subheadline": "Feuerwehr bricht Einsatzfahrt ab",
-	"date": "27.05.2018",
-	"text": "Um 13:53 h wurde der Ausrückebereich Ost zu einem Verkehrsunfall auf der Autobahn gerufen. Vor Ort stellte sich heraus, dass der Löschzug Lachen-Speyerdorf mit ausreichend Personal ausgerückt war, die LG Duttweiler und Geinsheim konnten ihre Alarmfahrt abbrechen.",
-	"url": "/a-bis-z/c/chronik/chronik-2018/hilfe-wird-nicht-benoetigt.html"
-}
-    ,
-{
-	"uid": "node-z323qebb571ox",
-	"headline": "Auf dem Weg zur Deutschen Meisterschaft",
-	"subheadline": "VfL-Schülerinnen gewinnen Regionsmeisterschaft",
-	"date": "26.05.2018",
-	"text": "Die Schülerinnen wurden ihrer Favoritenrolle gerecht und gewannen überlegen das Regionsturnier um den Einzug zum Turnier um die Deutsche Meisterschaft.",
-	"imageUrl": "https://www.duttweiler.de/_Resources/Persistent/1ad5e72846ed33dd6e4ed47bed7d2f5293cdfe09/18-05-27VfL-Sch%C3%BClerinnen-GewinnRegionsmeisterschaft-2.jpg",
-	"imageAlternativeText": "",
-	"url": "/a-bis-z/c/chronik/chronik-2018/vfl-schuelerinnen-auf-dem-weg-zur-deutschen-meisterschaft.html"
-}
-    ,
-{
-	"uid": "node-zeq8eq0ox9l08",
-	"headline": "Unterstützung bei Verkehrsunfall",
-	"subheadline": "Feuerwehr bei schwerem Unfall in Lachen-Speyerdorf",
-	"date": "20.05.2018",
-	"text": "Um 14:23h wurde die Feuerwehr Duttweiler zu einem Verkehrsunfall nach Lachen-Speyerdorf gerufen.",
-	"url": "/a-bis-z/c/chronik/chronik-2018/unterstuetzung-bei-verkehrsunfall.html"
-}
-    ,
-{
-	"uid": "node-y6v0xsbcn502z",
-	"headline": "Unfall auf L540",
-	"subheadline": "Mehrere Fahrzeuge beteiligt",
-	"date": "19.05.2018",
-	"text": "Zwischen Sportplatz und Ortseingang kam es zu einem Unfall mit mehreren Fahrzeugen. Weitere Informationen sind nicht bekannt.",
-	"url": "/a-bis-z/c/chronik/chronik-2018/unfall-auf-l540.html"
-}
-    ,
-{
-	"uid": "node-icmxwd2ugne59",
-	"headline": "Rauchmelder rettet Leben",
-	"subheadline": "Feuerwehr in Geinsheim im Einsatz",
-	"date": "18.05.2018",
-	"text": "Eine Bewohnerin in der Gäustraße  bemerkte am Freitagabend, dass die Rauchwarnmelder bei Ihrem Nachbarn  ausgelöst hatten und alarmierte um 22:16 h die Feuerwehr.",
-	"url": "/a-bis-z/c/chronik/chronik-2018/rauchmelder-rettet-leben.html"
-}
-    ,
-{
-	"uid": "node-endbuxdh8ew3s",
-	"headline": "Feuerwehr feiert",
-	"subheadline": "Zwei Tage Programm",
-	"date": "12.05.2018",
-	"text": "",
-	"url": "/a-bis-z/c/chronik/chronik-2018/feuerwehr-feiert.html"
-}
-    ,
-{
-	"uid": "node-5798c45c0dd9e",
-	"headline": "VDP.Maispitzen",
-	"subheadline": "Jahrgangspräsentation im Weingut Bergdolt",
-	"date": "12.05.2018",
-	"text": "",
-	"imageUrl": "https://www.duttweiler.de/_Resources/Persistent/a8287d236bf637a36e70b3a8e0a6f701f750f189/18-05-12VDP-Maispitzen-Bergdolt.jpg",
-	"imageAlternativeText": "",
-	"url": "/a-bis-z/c/chronik/chronik-2018/vdp-maispitzen.html"
-}
+  {
+    "uid": "node-psrtl3h7wnw7m",
+    "headline": "Im Bezirk ist der VfL Spitze",
+    "subheadline": "TT-Bezirkseinzelmeisterschaften Schüler/Schülerinnen",
+    "date": "16.09.2018",
+    "text": "Eine klare Angelegenheit waren die Bezirkseinzelmeisterschaften der Schüler und Schülerinnen A in Edenkoben.",
+    "url": "/a-bis-z/c/chronik/chronik-2018/im-bezirk-ist-der-vfl-spitze.html"
+  },
+  {
+    "uid": "node-q6h0rovszcw9z",
+    "headline": "Eingreifen nicht mehr erforderlich",
+    "subheadline": "Feuerwehr bei Notfalltüröffnung in Geinsheim",
+    "date": "09.09.2018",
+    "text": "Eine Notfalltüröffnung sorgte am frühen Sonntagnachmittag für einen Feuerwehreinsatz in Geinsheim.",
+    "url": "/a-bis-z/c/chronik/chronik-2018/eingreifen-nicht-mehr-erforderlich.html"
+  },
+  {
+    "uid": "node-qagvyipu2d8h9",
+    "headline": "Wieder kurze Nacht für Feuerwehr",
+    "subheadline": "Erneut Falschmeldung durch Brandmelder",
+    "date": "08.09.2018",
+    "text": "Wie schon am 7. September wurde der Ausrückebereich Neustadt–Ost mit der Löschgruppe Duttweiler um 3:09h durch eine ausgelöste Brandmeldeanlage nach Lachen-Speyerdorf alarmiert.",
+    "url": "/a-bis-z/c/chronik/chronik-2018/wieder-kurze-nacht-fuer-feuerwehr.html"
+  },
+  {
+    "uid": "node-hk1p4m8nbfks1",
+    "headline": "Technisches Problem beendet Nachruhe",
+    "subheadline": "Feuerwehr in Lachen-Speyerdorf",
+    "date": "07.09.2018",
+    "text": "Um 0:44h war die nacht für die Einsatzkräfte der Feuerwehr zu Ende.",
+    "url": "/a-bis-z/c/chronik/chronik-2018/technisches-problem-beendet-nachruhe.html"
+  },
+  {
+    "uid": "node-94gl04ggxfo71",
+    "headline": "Duttweilerer vorne dabei",
+    "subheadline": "TT-TOP-12-Turniere",
+    "date": "02.09.2018",
+    "text": "Bei den TOP-12-Turnieren der Schülerinnen und Schüler in Altrip haben sich die Vertreter aus Duttweiler wieder gut positioniert.",
+    "url": "/a-bis-z/c/chronik/chronik-2018/duttweilerer-vorne-dabei.html"
+  },
+  {
+    "uid": "node-oulo4u2twl334",
+    "headline": "Vier Ecowinner aus Duttweiler",
+    "subheadline": "Bioweine von Mohr-Gutting und Bergdolt ausgezeichnet",
+    "date": "29.08.2018",
+    "text": "Bei der 22. Auflage der EcoWinner-Weinprämierung zeigte sich erneut das hohe Qualitätsniveau der deutschen Bio-Weine.",
+    "imageUrl": "https://www.duttweiler.de/_Resources/Persistent/fbd2ad46cb85268ea92c03c61e9577c25d3d452b/EcovinLogo.jpg",
+    "imageAlternativeText": "",
+    "url": "/a-bis-z/c/chronik/chronik-2018/vier-ecowinner-aus-duttweiler.html"
+  },
+  {
+    "uid": "node-3525v7dkknkn8",
+    "headline": "Meister und Vizemeister aus Duttweiler",
+    "subheadline": "TT-Bezirkseinzelmeisterschaften",
+    "date": "26.08.2018",
+    "text": "Anna-Sophie Ebert vom VfL Duttweiler war die überragende Spielerin bei den Bezirkseinzelmeisterschaften der Schülerinnen B in Wörth.",
+    "url": "/a-bis-z/c/chronik/chronik-2018/meister-und-vizemeister-aus-duttweiler.html"
+  },
+  {
+    "uid": "node-dmixlhwsvylr9",
+    "headline": "Rettung aus dem Kühlschrank",
+    "subheadline": "Notfalldose bei der Ortsverwaltung erhältlich",
+    "date": "17.08.2018",
+    "text": "Im Ernstfall zählt jede Sekunde. Damit sowohl Rettungskräfte als auch Ärztinnen und Ärzte Personen in Not schnellstmöglich helfen können",
+    "imageUrl": "https://www.duttweiler.de/_Resources/Persistent/96b236044b5b3d0aed2a86c4d29efd3ceab9cadd/18-08-xxNotfalldose.jpg",
+    "imageAlternativeText": "",
+    "url": "/a-bis-z/c/chronik/chronik-2018/rettung-aus-dem-kuehlschrank.html"
+  },
+  {
+    "uid": "node-ejwooo6blb415",
+    "headline": "Kurz und heftig",
+    "subheadline": "Unwetter zieht über Duttweiler",
+    "date": "09.08.2018",
+    "text": "Der Wetterdienst hatte es prognostiziert und es kam aus heiterem Himmel kurz und heftig.",
+    "url": "/a-bis-z/c/chronik/chronik-2018/kurz-und-heftig.html"
+  },
+  {
+    "uid": "node-b8f2um7a4y1ux",
+    "headline": "Helene in Mußbach",
+    "subheadline": "Eröffnung Johannes-Kerwe",
+    "date": "03.08.2018",
+    "text": "",
+    "url": "/a-bis-z/c/chronik/chronik-2018/helene-in-mussbach-1.html"
+  }
 ];
