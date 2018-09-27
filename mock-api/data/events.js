abca1f1acad1f9e963a247b9d3eb0c7ddb1733f6
Jan Syring-Lingenfelder <github@jan-sl.de>
2018-09-27 12:47:10 +0200

    test(mock-api): add events data

---
+++ mock-api/data/events.js
@@ -0,0 +1,320 @@
+export const events_items = 
+[
+  {
+    "title": "Seniorenausflug",
+    "host": "Ortsbeirat",
+    "location": "",
+    "promote": "1",
+    "link": "/a-bis-z/v/veranstaltungen/senioren-ausflug-september.html",
+    "startDate": "13.09.18 08:10",
+    "endDate": ""
+  },
+  {
+    "title": "Weinbergswanderung",
+    "host": "Weingut Spies",
+    "location": "",
+    "promote": "1",
+    "link": "/a-bis-z/v/veranstaltungen/weinbergswanderung.html",
+    "startDate": "15.09.18 14:00",
+    "endDate": ""
+  },
+  {
+    "title": "Sitzung Ortsbeirat",
+    "host": "",
+    "location": "Sitzungssaal",
+    "promote": "1",
+    "link": "/a-bis-z/v/veranstaltungen/ortsbeirat-sitzung.html",
+    "startDate": "18.09.18 19:30",
+    "endDate": ""
+  },
+  {
+    "title": "Ökumenisches Friendesgebet",
+    "host": "",
+    "location": "Protestantische Kirche",
+    "promote": "1",
+    "link": "/a-bis-z/v/veranstaltungen/protestantische-kirche.html",
+    "startDate": "21.09.18 19:00",
+    "endDate": ""
+  },
+  {
+    "title": "Federweißer im Weingut Spies",
+    "host": "Weingut Spies",
+    "location": "",
+    "promote": "1",
+    "link": "/a-bis-z/v/veranstaltungen/herbstausschank-im-weingut-spies-1.html",
+    "startDate": "22.09.18 17:00",
+    "endDate": ""
+  },
+  {
+    "title": "Wein-Genuss-Hoffest",
+    "host": "",
+    "location": "Weingut Zöller-Lagas",
+    "promote": "1",
+    "link": "/a-bis-z/v/veranstaltungen/zoeller-lagas-hoffest.html",
+    "startDate": "29.09.18 17:00",
+    "endDate": "29.09.18 23:55"
+  },
+  {
+    "title": "W.I.N.E. Festival (mit Duttweiler Beteiligung)",
+    "host": "",
+    "location": "Neustadt, Hetzelplatz",
+    "promote": "1",
+    "link": "/a-bis-z/v/veranstaltungen/winefestival.html",
+    "startDate": "02.10.18 17:00",
+    "endDate": ""
+  },
+  {
+    "title": "W.I.N.E. Festival - Jungwinzermatinee (mit Duttweiler Beteiligung)",
+    "host": "",
+    "location": "Neustadt, Hetzelplatz",
+    "promote": "1",
+    "link": "/a-bis-z/v/veranstaltungen/winefestival.html",
+    "startDate": "07.10.18 11:00",
+    "endDate": "07.10.18 17:00"
+  },
+  {
+    "title": "Winzerfestumzug",
+    "host": "",
+    "location": "Neustadt",
+    "promote": "1",
+    "startDate": "14.10.18 14:00",
+    "endDate": ""
+  },
+  {
+    "title": "Winterarbeitsbesprechung",
+    "host": "Landfrauen",
+    "location": "Feuerwehrhaus",
+    "promote": "1",
+    "link": "/a-bis-z/v/veranstaltungen/landfrauenverein.html",
+    "startDate": "16.10.18 19:30",
+    "endDate": ""
+  },
+  {
+    "title": "Kurs",
+    "host": "Landfrauen",
+    "location": "Feuerwehrhaus",
+    "link": "/a-bis-z/v/veranstaltungen/landfrauenverein.html",
+    "startDate": "06.11.18 19:30",
+    "endDate": ""
+  },
+  {
+    "title": "Ortsbeirat Sitzung",
+    "host": "Ortsbeirat",
+    "location": "Sitzungssaal",
+    "link": "/a-bis-z/v/veranstaltungen/ortsbeirat-sitzung.html",
+    "startDate": "06.11.18 19:30",
+    "endDate": ""
+  },
+  {
+    "title": "Frauenweinabend",
+    "host": "Duttweiler Cuvée",
+    "location": "DGH",
+    "promote": "1",
+    "link": "/a-bis-z/v/veranstaltungen/frauenweinabend.html",
+    "startDate": "17.11.18 19:00",
+    "endDate": ""
+  },
+  {
+    "title": "Kurs",
+    "host": "Landfrauen",
+    "location": "Feuerwehrhaus",
+    "promote": "1",
+    "link": "/a-bis-z/v/veranstaltungen/landfrauenverein.html",
+    "startDate": "20.11.18 19:30",
+    "endDate": ""
+  },
+  {
+    "title": "Novembertage",
+    "host": "Weingut Bergdolt",
+    "location": "Weingut Bergdolt",
+    "promote": "1",
+    "startDate": "24.11.18 13:00",
+    "endDate": ""
+  },
+  {
+    "title": "Adventsmarkt",
+    "host": "VfL Duttweiler",
+    "location": "",
+    "promote": "1",
+    "link": "",
+    "startDate": "01.12.18 17:00",
+    "endDate": "03.12.17 22:55"
+  },
+  {
+    "title": "Vortrag",
+    "host": "Landfrauen",
+    "location": "",
+    "promote": "1",
+    "link": "/a-bis-z/v/veranstaltungen/landfrauenverein.html",
+    "startDate": "04.12.18 19:30",
+    "endDate": ""
+  },
+  {
+    "title": "Muschelessen - bitte reservieren",
+    "host": "VfL Duttweiler",
+    "location": "Sportheim",
+    "promote": "1",
+    "link": "/a-bis-z/v/veranstaltungen/muschelessen-beim-vfl.html",
+    "startDate": "06.12.17 18:00",
+    "endDate": ""
+  },
+  {
+    "title": "Gemeinsamer Start ins neue Jahr",
+    "host": "",
+    "location": "Dorfplatz",
+    "promote": "1",
+    "startDate": "01.01.19 00:00",
+    "endDate": ""
+  },
+  {
+    "title": "Neujahrsempfang",
+    "host": "Ortsbeirat",
+    "location": "Dorfgemeinschaftshaus",
+    "promote": "1",
+    "link": "/a-bis-z/v/veranstaltungen/neujahrsempfang.html",
+    "startDate": "02.01.19 19:00",
+    "endDate": ""
+  },
+  {
+    "title": "Seniorennachmittag",
+    "host": "Ortsbeirat",
+    "location": "Dorfgemeinschaftshaus",
+    "promote": "1",
+    "link": "/a-bis-z/v/veranstaltungen/senioren-bingo.html",
+    "startDate": "10.01.19 14:30",
+    "endDate": ""
+  },
+  {
+    "title": "Weihnachtsbaumsammlung",
+    "host": "Jugendfeuerwehr",
+    "location": "",
+    "promote": "1",
+    "link": "/a-bis-z/v/veranstaltungen/weihnachtsbaumsammlung.html",
+    "startDate": "12.01.19 09:00",
+    "endDate": ""
+  },
+  {
+    "title": "Schlachtfest",
+    "host": "Ortsbeirat",
+    "location": "Festhalle",
+    "promote": "1",
+    "link": "/a-bis-z/v/veranstaltungen/schlachtfest.html",
+    "startDate": "02.02.19 11:00",
+    "endDate": ""
+  },
+  {
+    "title": "Seniorennachmittag",
+    "host": "Ortsbeirat",
+    "location": "Dorfgemeinschaftshaus",
+    "promote": "1",
+    "link": "/a-bis-z/v/veranstaltungen/senioren-bingo.html",
+    "startDate": "14.02.19 14:30",
+    "endDate": ""
+  },
+  {
+    "title": "Stehkragenfastnacht",
+    "host": "Ortsbeirat",
+    "location": "Festhalle",
+    "promote": "1",
+    "link": "/a-bis-z/v/veranstaltungen/stehkragenfastnacht.html",
+    "startDate": "15.02.19 19:30",
+    "endDate": ""
+  },
+  {
+    "title": "Landfrauen: Kurs",
+    "host": "Landfrauenverein",
+    "location": "Dorfgemeinschaftshaus",
+    "promote": "1",
+    "link": "/a-bis-z/v/veranstaltungen/landfrauenverein.html",
+    "startDate": "",
+    "endDate": ""
+  },
+  {
+    "title": "Seniorennachmittag",
+    "host": "Ortsbeirat",
+    "location": "Dorfgemeinschaftshaus",
+    "promote": "1",
+    "link": "/a-bis-z/v/veranstaltungen/senioren-singkreis.html",
+    "startDate": "14.03.19 14:30",
+    "endDate": ""
+  },
+  {
+    "title": "Fass uff de Gass",
+    "host": "",
+    "location": "Dudostraße",
+    "promote": "1",
+    "link": "http://www.fass-uff-de-gass.de",
+    "startDate": "13.04.19 16:00",
+    "endDate": ""
+  },
+  {
+    "title": "Seniorennachmittag",
+    "host": "Ortsbeirat",
+    "location": "Dorfgemeinschaftshaus",
+    "promote": "1",
+    "link": "/a-bis-z/v/veranstaltungen/senioren-vortrag.html",
+    "startDate": "11.04.19 14:30",
+    "endDate": ""
+  },
+  {
+    "title": "Feierowend",
+    "host": "",
+    "location": "Weingut Breitling-Walter",
+    "promote": "1",
+    "link": "/a-bis-z/w/weinguter/breitling-walter/feierowend.html",
+    "startDate": "",
+    "endDate": ""
+  },
+  {
+    "title": "Probiertage",
+    "host": "",
+    "location": "Weingut Mohr-Gutting",
+    "promote": "1",
+    "link": "/a-bis-z/v/veranstaltungen/mohr-gutting-probiertage.html",
+    "startDate": "",
+    "endDate": ""
+  },
+  {
+    "title": "Seniorenausflug",
+    "host": "Ortsbeirat",
+    "location": "",
+    "promote": "1",
+    "link": "/a-bis-z/v/veranstaltungen/senioren-ausflug-mai.html",
+    "startDate": "",
+    "endDate": ""
+  },
+  {
+    "title": "VDP.Maispitzen - Jahrgangspräsentation",
+    "host": "",
+    "location": "Weingut Bergdolt - Klostergut St. Lamprecht",
+    "promote": "1",
+    "link": "/a-bis-z/v/veranstaltungen/bergdolt-klostergut.html",
+    "startDate": "",
+    "endDate": ""
+  },
+  {
+    "title": "1000PS - Feschd",
+    "host": "",
+    "location": "Wasserturm",
+    "promote": "1",
+    "startDate": "",
+    "endDate": ""
+  },
+  {
+    "title": "Picknick unterm Nussbaum",
+    "host": "Judith Gschwindt",
+    "location": "Rund um den Nussbaum",
+    "promote": "1",
+    "startDate": "26.05.19 11:00",
+    "endDate": ""
+  },
+  {
+    "title": "Duttweiler Weinblüten",
+    "host": "Duttweiler Winzer",
+    "location": "Dorfplatz",
+    "promote": "1",
+    "link": "http://static.duttweiler.de/feste/weinblueten/wb.htm",
+    "startDate": "01.06.19 17:00",
+    "endDate": ""
+  }
+];
--- mock-api/server.js
+++ mock-api/server.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import { news_items } from "./data/news";
+import { events_items } from "./data/events";
 const server = express();
 
 server.use((req, res, next) => {
@@ -7,5 +8,6 @@ server.use((req, res, next) => {
     next()
 })
 server.get('/news.json', (req, res) => res.send(news_items))
+server.get('/events.json', (req, res) => res.send(events_items))
 
 server.listen(3000, () => console.log('Dummy API listening on port 3000'))
