97c8ebd1e5c21ea7a63f72bb6acd308276f70b86
Jan Syring-Lingenfelder <github@jan-sl.de>
2018-12-30 15:14:18 +0100

    test: add seperate data set for unit tests

---
+++ test/unit/data/events.js
@@ -0,0 +1,177 @@
+export const list = 
+[
+  {
+    "title": "Neujahrsempfang",
+    "host": "Ortsbeirat",
+    "location": "Dorfgemeinschaftshaus",
+    "promote": "1",
+    "link": "/a-bis-z/v/veranstaltungen/neujahrsempfang.html",
+    "startDate": "02.01.19 19:00",
+    "endDate": ""
+  },
+  {
+    "title": "Seniorennachmittag",
+    "host": "Ortsbeirat",
+    "location": "Dorfgemeinschaftshaus",
+    "promote": "1",
+    "link": "/a-bis-z/v/veranstaltungen/senioren-bingo.html",
+    "startDate": "10.01.19 14:30",
+    "endDate": ""
+  },
+  {
+    "title": "Weihnachtsbaumsammlung",
+    "host": "Jugendfeuerwehr",
+    "location": "",
+    "promote": "1",
+    "link": "/a-bis-z/v/veranstaltungen/weihnachtsbaumsammlung.html",
+    "startDate": "12.01.19 09:00",
+    "endDate": ""
+  },
+  {
+    "title": "Schlachtfest",
+    "host": "Ortsbeirat",
+    "location": "Festhalle",
+    "promote": "1",
+    "link": "/a-bis-z/v/veranstaltungen/schlachtfest.html",
+    "startDate": "02.02.19 11:00",
+    "endDate": ""
+  },
+  {
+    "title": "Seniorennachmittag",
+    "host": "Ortsbeirat",
+    "location": "Dorfgemeinschaftshaus",
+    "promote": "1",
+    "link": "/a-bis-z/v/veranstaltungen/senioren-bingo.html",
+    "startDate": "14.02.19 14:30",
+    "endDate": ""
+  },
+  {
+    "title": "Stehkragenfastnacht",
+    "host": "Ortsbeirat",
+    "location": "Festhalle",
+    "promote": "1",
+    "link": "/a-bis-z/v/veranstaltungen/stehkragenfastnacht.html",
+    "startDate": "15.02.19 19:30",
+    "endDate": ""
+  },
+  {
+    "title": "Landfrauen: Kurs",
+    "host": "Landfrauenverein",
+    "location": "Dorfgemeinschaftshaus",
+    "promote": "1",
+    "link": "/a-bis-z/v/veranstaltungen/landfrauenverein.html",
+    "startDate": "",
+    "endDate": ""
+  },
+  {
+    "title": "Seniorennachmittag",
+    "host": "Ortsbeirat",
+    "location": "Dorfgemeinschaftshaus",
+    "promote": "1",
+    "link": "/a-bis-z/v/veranstaltungen/senioren-singkreis.html",
+    "startDate": "14.03.19 14:30",
+    "endDate": ""
+  },
+  {
+    "title": "Fass uff de Gass",
+    "host": "",
+    "location": "Dudostraße",
+    "promote": "1",
+    "link": "http://www.fass-uff-de-gass.de",
+    "startDate": "13.04.19 16:00",
+    "endDate": ""
+  },
+  {
+    "title": "Seniorennachmittag",
+    "host": "Ortsbeirat",
+    "location": "Dorfgemeinschaftshaus",
+    "promote": "1",
+    "link": "/a-bis-z/v/veranstaltungen/senioren-vortrag.html",
+    "startDate": "11.04.19 14:30",
+    "endDate": ""
+  },
+  {
+    "title": "Feierowend",
+    "host": "",
+    "location": "Weingut Breitling-Walter",
+    "promote": "1",
+    "link": "/a-bis-z/w/weinguter/breitling-walter/feierowend.html",
+    "startDate": "",
+    "endDate": ""
+  },
+  {
+    "title": "Probiertage",
+    "host": "",
+    "location": "Weingut Mohr-Gutting",
+    "promote": "1",
+    "link": "/a-bis-z/v/veranstaltungen/mohr-gutting-probiertage.html",
+    "startDate": "",
+    "endDate": ""
+  },
+  {
+    "title": "Seniorenausflug",
+    "host": "Ortsbeirat",
+    "location": "",
+    "promote": "1",
+    "link": "/a-bis-z/v/veranstaltungen/senioren-ausflug-mai.html",
+    "startDate": "",
+    "endDate": ""
+  },
+  {
+    "title": "VDP.Maispitzen - Jahrgangspräsentation",
+    "host": "",
+    "location": "Weingut Bergdolt - Klostergut St. Lamprecht",
+    "promote": "1",
+    "link": "/a-bis-z/v/veranstaltungen/bergdolt-klostergut.html",
+    "startDate": "",
+    "endDate": ""
+  },
+  {
+    "title": "1000PS - Feschd",
+    "host": "",
+    "location": "Wasserturm",
+    "promote": "1",
+    "startDate": "",
+    "endDate": ""
+  },
+  {
+    "title": "Picknick unterm Nussbaum",
+    "host": "Judith Gschwindt",
+    "location": "Rund um den Nussbaum",
+    "promote": "1",
+    "startDate": "26.05.19 11:00",
+    "endDate": ""
+  },
+  {
+    "title": "Duttweiler Weinblüten",
+    "host": "Duttweiler Winzer",
+    "location": "Dorfplatz",
+    "promote": "1",
+    "link": "http://static.duttweiler.de/feste/weinblueten/wb.htm",
+    "startDate": "01.06.19 17:00",
+    "endDate": ""
+  }
+];
+
+export const itemWithLink = [
+  {
+    "title": "Duttweiler Weinblüten",
+    "host": "Duttweiler Winzer",
+    "location": "Dorfplatz",
+    "promote": "1",
+    "link": "http://static.duttweiler.de/feste/weinblueten/wb.htm",
+    "startDate": "01.06.19 17:00",
+    "endDate": ""
+  }
+]
+
+export const itemWithoutLink = [
+  {
+    "title": "Picknick unterm Nussbaum",
+    "host": "Judith Gschwindt",
+    "location": "Rund um den Nussbaum",
+    "promote": "1",
+    "startDate": "26.05.19 11:00",
+    "endDate": ""
+  },
+]
---
+++ test/unit/data/news.js
@@ -0,0 +1,111 @@
+export const list = 
+[
+  {
+    "uid": "node-psrtl3h7wnw7m",
+    "headline": "Im Bezirk ist der VfL Spitze",
+    "subheadline": "TT-Bezirkseinzelmeisterschaften Schüler/Schülerinnen",
+    "date": "16.09.2018",
+    "text": "Eine klare Angelegenheit waren die Bezirkseinzelmeisterschaften der Schüler und Schülerinnen A in Edenkoben.",
+    "url": "/a-bis-z/c/chronik/chronik-2018/im-bezirk-ist-der-vfl-spitze.html"
+  },
+  {
+    "uid": "node-q6h0rovszcw9z",
+    "headline": "Eingreifen nicht mehr erforderlich",
+    "subheadline": "Feuerwehr bei Notfalltüröffnung in Geinsheim",
+    "date": "09.09.2018",
+    "text": "Eine Notfalltüröffnung sorgte am frühen Sonntagnachmittag für einen Feuerwehreinsatz in Geinsheim.",
+    "url": "/a-bis-z/c/chronik/chronik-2018/eingreifen-nicht-mehr-erforderlich.html"
+  },
+  {
+    "uid": "node-qagvyipu2d8h9",
+    "headline": "Wieder kurze Nacht für Feuerwehr",
+    "subheadline": "Erneut Falschmeldung durch Brandmelder",
+    "date": "08.09.2018",
+    "text": "Wie schon am 7. September wurde der Ausrückebereich Neustadt–Ost mit der Löschgruppe Duttweiler um 3:09h durch eine ausgelöste Brandmeldeanlage nach Lachen-Speyerdorf alarmiert.",
+    "url": "/a-bis-z/c/chronik/chronik-2018/wieder-kurze-nacht-fuer-feuerwehr.html"
+  },
+  {
+    "uid": "node-hk1p4m8nbfks1",
+    "headline": "Technisches Problem beendet Nachruhe",
+    "subheadline": "Feuerwehr in Lachen-Speyerdorf",
+    "date": "07.09.2018",
+    "text": "Um 0:44h war die nacht für die Einsatzkräfte der Feuerwehr zu Ende.",
+    "url": "/a-bis-z/c/chronik/chronik-2018/technisches-problem-beendet-nachruhe.html"
+  },
+  {
+    "uid": "node-94gl04ggxfo71",
+    "headline": "Duttweilerer vorne dabei",
+    "subheadline": "TT-TOP-12-Turniere",
+    "date": "02.09.2018",
+    "text": "Bei den TOP-12-Turnieren der Schülerinnen und Schüler in Altrip haben sich die Vertreter aus Duttweiler wieder gut positioniert.",
+    "url": "/a-bis-z/c/chronik/chronik-2018/duttweilerer-vorne-dabei.html"
+  },
+  {
+    "uid": "node-oulo4u2twl334",
+    "headline": "Vier Ecowinner aus Duttweiler",
+    "subheadline": "Bioweine von Mohr-Gutting und Bergdolt ausgezeichnet",
+    "date": "29.08.2018",
+    "text": "Bei der 22. Auflage der EcoWinner-Weinprämierung zeigte sich erneut das hohe Qualitätsniveau der deutschen Bio-Weine.",
+    "imageUrl": "https://www.duttweiler.de/_Resources/Persistent/fbd2ad46cb85268ea92c03c61e9577c25d3d452b/EcovinLogo.jpg",
+    "imageAlternativeText": "",
+    "url": "/a-bis-z/c/chronik/chronik-2018/vier-ecowinner-aus-duttweiler.html"
+  },
+  {
+    "uid": "node-3525v7dkknkn8",
+    "headline": "Meister und Vizemeister aus Duttweiler",
+    "subheadline": "TT-Bezirkseinzelmeisterschaften",
+    "date": "26.08.2018",
+    "text": "Anna-Sophie Ebert vom VfL Duttweiler war die überragende Spielerin bei den Bezirkseinzelmeisterschaften der Schülerinnen B in Wörth.",
+    "url": "/a-bis-z/c/chronik/chronik-2018/meister-und-vizemeister-aus-duttweiler.html"
+  },
+  {
+    "uid": "node-dmixlhwsvylr9",
+    "headline": "Rettung aus dem Kühlschrank",
+    "subheadline": "Notfalldose bei der Ortsverwaltung erhältlich",
+    "date": "17.08.2018",
+    "text": "Im Ernstfall zählt jede Sekunde. Damit sowohl Rettungskräfte als auch Ärztinnen und Ärzte Personen in Not schnellstmöglich helfen können",
+    "imageUrl": "https://www.duttweiler.de/_Resources/Persistent/96b236044b5b3d0aed2a86c4d29efd3ceab9cadd/18-08-xxNotfalldose.jpg",
+    "imageAlternativeText": "",
+    "url": "/a-bis-z/c/chronik/chronik-2018/rettung-aus-dem-kuehlschrank.html"
+  },
+  {
+    "uid": "node-ejwooo6blb415",
+    "headline": "Kurz und heftig",
+    "subheadline": "Unwetter zieht über Duttweiler",
+    "date": "09.08.2018",
+    "text": "Der Wetterdienst hatte es prognostiziert und es kam aus heiterem Himmel kurz und heftig.",
+    "url": "/a-bis-z/c/chronik/chronik-2018/kurz-und-heftig.html"
+  },
+  {
+    "uid": "node-b8f2um7a4y1ux",
+    "headline": "Helene in Mußbach",
+    "subheadline": "Eröffnung Johannes-Kerwe",
+    "date": "03.08.2018",
+    "text": "",
+    "url": "/a-bis-z/c/chronik/chronik-2018/helene-in-mussbach-1.html"
+  }
+];
+
+export const itemWithAnImage = [
+  {
+    "uid": "node-oulo4u2twl334",
+    "headline": "Vier Ecowinner aus Duttweiler",
+    "subheadline": "Bioweine von Mohr-Gutting und Bergdolt ausgezeichnet",
+    "date": "29.08.2018",
+    "text": "Bei der 22. Auflage der EcoWinner-Weinprämierung zeigte sich erneut das hohe Qualitätsniveau der deutschen Bio-Weine.",
+    "imageUrl": "https://www.duttweiler.de/_Resources/Persistent/fbd2ad46cb85268ea92c03c61e9577c25d3d452b/EcovinLogo.jpg",
+    "imageAlternativeText": "Ecovin Logo",
+    "url": "/a-bis-z/c/chronik/chronik-2018/vier-ecowinner-aus-duttweiler.html"
+  },
+];
+
+export const itemWithoutAnImage = [
+  {
+    "uid": "node-oulo4u2twl334",
+    "headline": "Vier Ecowinner aus Duttweiler",
+    "subheadline": "Bioweine von Mohr-Gutting und Bergdolt ausgezeichnet",
+    "date": "29.08.2018",
+    "text": "Bei der 22. Auflage der EcoWinner-Weinprämierung zeigte sich erneut das hohe Qualitätsniveau der deutschen Bio-Weine.",
+    "url": "/a-bis-z/c/chronik/chronik-2018/vier-ecowinner-aus-duttweiler.html"
+  },
+];
